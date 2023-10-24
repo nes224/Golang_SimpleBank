8707359221560ece13516c71fbaf0fc71ec5d5f1
Nes224 <Nes224@hotmail.com>
2023-10-24 15:41:38 +0700

    account.go

---
+++ api/account.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"net/http"
+	"github.com/techschool/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountReqyest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountReqyest
+	if err := ctx.ShouldBindJSON(req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner: req.Owner,
+		Currency: req.Currency,
+		Balance: 0,
+	}
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse((err)))
+		return 
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
